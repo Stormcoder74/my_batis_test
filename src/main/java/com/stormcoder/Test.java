1eb5cfdf03d37bcb98819c09b6b242b663b7b32d
Teryaev Sergey <teryaevs@yandex.ru>
2019-10-04 11:21:14 +0300

    create repository

---
+++ src/main/java/com/stormcoder/Test.java
@@ -0,0 +1,4 @@
+package com.stormcoder;
+
+public class Test {
+}
---
+++ src/main/java/com/stormcoder/mapper/SubscriberMapper.java
@@ -0,0 +1,8 @@
+package com.stormcoder.mapper;
+
+import java.util.List;
+
+public interface SubscriberMapper {
+    Subscriber getSubscriberById(Integer id);
+    List getSubscriber();
+}
